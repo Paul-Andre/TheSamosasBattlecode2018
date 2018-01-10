afaeef2a8fe23045b7faad71055810ad00f81249
Anass Al <mr@anassinator.com>
2018-01-09 20:49:21 -0500

    Update clang-format style

--- tests/distances.cpp
+++ tests/distances.cpp
@@ -14,57 +14,57 @@ int dy[8] = {1, 0, -1, -1, -1, 0, 1, 1};
 typedef pair<int, int> pii;
 
 int main() {
-    memset(distances, -1, sizeof(distances));
+  memset(distances, -1, sizeof(distances));
 
-    cin >> n >> m;
+  cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> coll[i][j];
-        }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> coll[i][j];
     }
+  }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (coll[i][j]) continue;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (coll[i][j]) continue;
 
-            memset(vis, 0, sizeof(vis));
+      memset(vis, 0, sizeof(vis));
 
-            queue<pii> q;
+      queue<pii> q;
 
-            q.push(pii(i, j));
-            vis[i][j] = true;
+      q.push(pii(i, j));
+      vis[i][j] = true;
 
-            short dist = 0;
+      short dist = 0;
 
-            while (!q.empty()) {
-                size_t size = q.size();
+      while (!q.empty()) {
+        size_t size = q.size();
 
-                for (int k = 0; k < size; k++) {
-                    pii current = q.front();
-                    q.pop();
+        for (int k = 0; k < size; k++) {
+          pii current = q.front();
+          q.pop();
 
-                    int ii = current.first;
-                    int jj = current.second;
+          int ii = current.first;
+          int jj = current.second;
 
-                    distances[i][j][ii][jj] = dist;
+          distances[i][j][ii][jj] = dist;
 
-                    for (int k = 0; k < 8; k++) {
-                        int x = ii + dx[k];
-                        int y = jj + dy[k];
+          for (int k = 0; k < 8; k++) {
+            int x = ii + dx[k];
+            int y = jj + dy[k];
 
-                        if (x >= 0 && x < n && y >= 0 && y < m && !vis[x][y] &&
-                            !coll[x][y]) {
-                            q.push(pii(x, y));
-                            vis[x][y] = true;
-                        }
-                    }
-                }
-
-                dist++;
+            if (x >= 0 && x < n && y >= 0 && y < m && !vis[x][y] &&
+                !coll[x][y]) {
+              q.push(pii(x, y));
+              vis[x][y] = true;
             }
+          }
         }
-    }
 
-    cerr << "Done\n";
+        dist++;
+      }
+    }
+  }
+
+  cerr << "Done\n";
 }
