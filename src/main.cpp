1dde34ace530eb30f9957ac3c9c431f7b43d10e4
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-01-12 18:04:22 -0500

    Randomly replicating

--- src/main.cpp
+++ src/main.cpp
@@ -7,54 +7,60 @@ using namespace std;
 using namespace bc;
 
 int main() {
-    printf("Player C bot starting\n");
+  printf("Player C bot starting\n");
 
-    // It's good to try and make matches deterministic. It's not required, but it
-    // makes debugging wayyy easier.
-    // Now if you use random() it will produce the same output each map.
-    srand(0);
+  // It's good to try and make matches deterministic. It's not required, but it
+  // makes debugging wayyy easier.
+  // Now if you use random() it will produce the same output each map.
+  srand(0);
 
-    // we provide some helpful helpers :)
-    Direction dir = North;
-    Direction opposite = direction_opposite(dir);
+  // we provide some helpful helpers :)
+  Direction dir = North;
+  Direction opposite = direction_opposite(dir);
 
-    printf("Opposite direction of %d: %d\n", dir, opposite);
+  printf("Opposite direction of %d: %d\n", dir, opposite);
 
-    // Make sure that the world is sane!
-    assert(opposite == South);
+  // Make sure that the world is sane!
+  assert(opposite == South);
 
-    printf("Connecting to manager...\n");
+  printf("Connecting to manager...\n");
 
-    // Most methods return pointers; methods returning integers or enums are the only exception.
-    GameController gc;
+  // Most methods return pointers; methods returning integers or enums are the only exception.
+  GameController gc;
 
-    printf("Connected!\n");
+  printf("Connected!\n");
 
-    // loop through the whole game.
-    while (true) {
-        // The API is "object-oriented" - most methods take pointers to some object.
-        uint32_t round = gc.get_round();
-        printf("Round: %d\n", round);
+  // loop through the whole game.
+  while (true) {
+    // The API is "object-oriented" - most methods take pointers to some object.
+    uint32_t round = gc.get_round();
+    printf("Round: %d\n", round);
 
-        // Note that all operations perform copies out of their data structures, returning new objects.
-        // You're responsible for freeing objects.
-        vector<Unit> units = gc.get_my_units();
+    // Note that all operations perform copies out of their data structures, returning new objects.
+    // You're responsible for freeing objects.
+    vector<Unit> units = gc.get_my_units();
 
-        // it's good to cache things like this. Calls into the API have around a 20ns overhead, plus the cost of
-        // copying the data out of the engine. not horrible, but good to avoid more than necessary.
-        for (size_t i = 0; i < units.size(); i++) {
-            Unit &unit = units[i];
+    // it's good to cache things like this. Calls into the API have around a 20ns overhead, plus the cost of
+    // copying the data out of the engine. not horrible, but good to avoid more than necessary.
+    for (size_t i = 0; i < units.size(); i++) {
+      Unit &unit = units[i];
 
-            // Calls on the controller take unit IDs for ownership reasons.
-            uint16_t id = unit.get_id();
-            if (gc.can_move(id, North) && gc.is_move_ready(id)) {
-                gc.move_robot(id, North);
-            }
+      // Calls on the controller take unit IDs for ownership reasons.
+      uint16_t id = unit.get_id();
+      Direction random_direction = Direction(rand() % 8);
+      if (gc.can_move(id, random_direction) && gc.is_move_ready(id)) {
+        gc.move_robot(id, random_direction);
+      }
 
-        }
+      random_direction = Direction(rand() % 8);
+      if (gc.can_replicate(id, random_direction)) {
+        gc.replicate(id, random_direction);
+      }
 
-        fflush(stdout);
-
-        gc.next_turn();
     }
+
+    fflush(stdout);
+
+    gc.next_turn();
+  }
 }
