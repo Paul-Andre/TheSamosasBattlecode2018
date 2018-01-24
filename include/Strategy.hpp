7d789c6ff203e7ca45753e68caaff7ceb54cd86e
Anass Al <mr@anassinator.com>
2018-01-23 19:24:32 -0500

    Add preliminary BuildingStrategy implementation

--- include/Strategy.hpp
+++ include/Strategy.hpp
@@ -284,31 +284,34 @@ class WorkerRushStrategy : public WorkerStrategy {
 
   void set_should_replicate(bool status) { should_replicate = status; }
 
-  vector<unsigned> random_move_order(GameState &game_state, unordered_set<unsigned> unmovable) {
-
-    vector<vector<bool>> visited(game_state.map_info.width, vector<bool>(game_state.map_info.height, false));
-    queue<pair<int,int>> q;
+  vector<unsigned> random_move_order(GameState &game_state,
+                                     unordered_set<unsigned> unmovable) {
+    vector<vector<bool>> visited(
+        game_state.map_info.width,
+        vector<bool>(game_state.map_info.height, false));
+    queue<pair<int, int>> q;
     vector<unsigned> units_to_be_moved;
 
-    vector<pair<int,int>> initial;
+    vector<pair<int, int>> initial;
 
-    for (int i=0; i<game_state.map_info.width; i++) {
-      for (int j=0; j<game_state.map_info.height; j++) {
-        if (!game_state.has_unit_at(i,j) && game_state.map_info.passable_terrain[i][j]) {
+    for (int i = 0; i < game_state.map_info.width; i++) {
+      for (int j = 0; j < game_state.map_info.height; j++) {
+        if (!game_state.has_unit_at(i, j) &&
+            game_state.map_info.passable_terrain[i][j]) {
           visited[i][j] = true;
-          initial.push_back(make_pair(i,j));
+          initial.push_back(make_pair(i, j));
         }
       }
     }
 
     random_shuffle(initial.begin(), initial.end());
 
-    for(int i=0; i<initial.size(); i++) {
+    for (int i = 0; i < initial.size(); i++) {
       q.push(initial[i]);
     }
 
     while (!q.empty()) {
-      pair<int,int> current = q.front();
+      pair<int, int> current = q.front();
       q.pop();
 
       int ii = current.first;
@@ -318,15 +321,13 @@ class WorkerRushStrategy : public WorkerStrategy {
         int x = ii + constants::DX[k];
         int y = jj + constants::DY[k];
 
-
-        if (x >= 0 && x < game_state.map_info.width && y >= 0 && y < game_state.map_info.width
-            && !visited[x][y] &&
-            game_state.my_units.is_occupied[x][y] ) {
-
+        if (x >= 0 && x < game_state.map_info.width && y >= 0 &&
+            y < game_state.map_info.width && !visited[x][y] &&
+            game_state.my_units.is_occupied[x][y]) {
           auto id = game_state.my_units.by_location[x][y];
           if (unmovable.count(id) != 0) continue;
 
-          if(!game_state.gc.is_move_ready(id)) continue;
+          if (!game_state.gc.is_move_ready(id)) continue;
 
           units_to_be_moved.push_back(id);
           q.push(make_pair(x, y));
@@ -376,11 +377,11 @@ class WorkerRushStrategy : public WorkerStrategy {
                                should_replicate);
     }
 
-    vector<unsigned> units_to_be_moved_randomly = random_move_order(game_state, targetting);
+    vector<unsigned> units_to_be_moved_randomly =
+        random_move_order(game_state, targetting);
 
     for (const auto id : units_to_be_moved_randomly) {
-      maybe_move_and_replicate_randomly(game_state, id,
-          should_replicate);
+      maybe_move_and_replicate_randomly(game_state, id, should_replicate);
     }
   }
 };
@@ -392,7 +393,11 @@ class BuildingStrategy : public WorkerStrategy {
  public:
   BuildingStrategy(const UnitType &unit_type) : unit_type(unit_type) {}
 
-  void run(GameState &game_state, unordered_set<unsigned> workers) {}
+  void run(GameState &game_state, unordered_set<unsigned> workers) {
+    for (const auto worker_id : workers) {
+      if (maybe_blueprint(game_state, worker_id, unit_type)) return;
+    }
+  }
 };
 
 class RocketBoardingStrategy : public RobotStrategy {
