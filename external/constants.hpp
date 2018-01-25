99574a0249ea5cf9c2cddd9a58bf7f4607f9070b
Anass Al <mr@anassinator.com>
2018-01-25 02:06:33 -0500

    Move from C-style arrays to std::array

--- external/constants.hpp
+++ external/constants.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <limits>
 #include <unordered_set>
 #include <vector>
@@ -31,14 +32,11 @@ const static unordered_set<unsigned> STRUCTURE_TYPES = {Factory, Rocket};
 // Directions.
 constexpr static int N_DIRECTIONS = 9;
 constexpr static int N_DIRECTIONS_WITHOUT_CENTER = N_DIRECTIONS - 1;
-constexpr static int DX[N_DIRECTIONS] = {0, 1, 1, 1, 0, -1, -1, -1, 0};
-constexpr static int DY[N_DIRECTIONS] = {1, 1, 0, -1, -1, -1, 0, 1, 0};
-constexpr static bc::Direction DIRECTIONS[N_DIRECTIONS] = {
-    North,     Northeast, East,      Southeast, South,
-    Southwest, West,      Northwest, Center};
+constexpr static array<int, N_DIRECTIONS> DX = {0, 1, 1, 1, 0, -1, -1, -1, 0};
+constexpr static array<int, N_DIRECTIONS> DY = {1, 1, 0, -1, -1, -1, 0, 1, 0};
 
 // Costs.
-const static unsigned BLUEPRINT_COST[N_UNIT_TYPES] = {
+const static array<unsigned, N_UNIT_TYPES> BLUEPRINT_COST = {
     numeric_limits<unsigned>::max(),  // Worker
     numeric_limits<unsigned>::max(),  // Knight
     numeric_limits<unsigned>::max(),  // Ranger
@@ -47,7 +45,7 @@ const static unsigned BLUEPRINT_COST[N_UNIT_TYPES] = {
     bc::unit_type_get_blueprint_cost(Factory),
     bc::unit_type_get_blueprint_cost(Rocket),
 };
-const static unsigned FACTORY_COST[N_UNIT_TYPES] = {
+const static array<unsigned, N_UNIT_TYPES> FACTORY_COST = {
     bc::unit_type_get_factory_cost(Worker),
     bc::unit_type_get_factory_cost(Knight),
     bc::unit_type_get_factory_cost(Ranger),
@@ -59,7 +57,7 @@ const static unsigned FACTORY_COST[N_UNIT_TYPES] = {
 const static unsigned REPLICATION_COST = bc::unit_type_get_replicate_cost();
 
 // Values.
-const static unsigned UNIT_VALUES[N_UNIT_TYPES] = {
+const static array<unsigned, N_UNIT_TYPES> UNIT_VALUES = {
     bc::unit_type_get_value(Worker), bc::unit_type_get_value(Knight),
     bc::unit_type_get_value(Ranger), bc::unit_type_get_value(Mage),
     bc::unit_type_get_value(Healer), bc::unit_type_get_value(Factory),
@@ -68,51 +66,52 @@ const static unsigned UNIT_VALUES[N_UNIT_TYPES] = {
 
 // Research.
 constexpr static int N_RESEARCH_LEVELS = 4;
-const static unsigned RESEARCH_COST[N_UNIT_TYPES][N_RESEARCH_LEVELS] = {
-    {
-        cost_of(Worker, 1),
-        cost_of(Worker, 2),
-        cost_of(Worker, 3),
-        cost_of(Worker, 4),
-    },
-    {
-        cost_of(Knight, 1),
-        cost_of(Knight, 2),
-        cost_of(Knight, 3),
-        numeric_limits<unsigned>::max(),
-    },
-    {
-        cost_of(Ranger, 1),
-        cost_of(Ranger, 2),
-        cost_of(Ranger, 3),
-        numeric_limits<unsigned>::max(),
-    },
-    {
-        cost_of(Mage, 1),
-        cost_of(Mage, 2),
-        cost_of(Mage, 3),
-        cost_of(Mage, 4),
-    },
-    {
-        cost_of(Healer, 1),
-        cost_of(Healer, 2),
-        cost_of(Healer, 3),
-        numeric_limits<unsigned>::max(),
-    },
-    {
-        // Can't research factories.
-        numeric_limits<unsigned>::max(),
-        numeric_limits<unsigned>::max(),
-        numeric_limits<unsigned>::max(),
-        numeric_limits<unsigned>::max(),
-    },
-    {
-        cost_of(Rocket, 1),
-        cost_of(Rocket, 2),
-        cost_of(Rocket, 3),
-        numeric_limits<unsigned>::max(),
-    },
-};
+const static array<array<unsigned, N_RESEARCH_LEVELS>, N_UNIT_TYPES>
+    RESEARCH_COST = {{
+        {
+            cost_of(Worker, 1),
+            cost_of(Worker, 2),
+            cost_of(Worker, 3),
+            cost_of(Worker, 4),
+        },
+        {
+            cost_of(Knight, 1),
+            cost_of(Knight, 2),
+            cost_of(Knight, 3),
+            numeric_limits<unsigned>::max(),
+        },
+        {
+            cost_of(Ranger, 1),
+            cost_of(Ranger, 2),
+            cost_of(Ranger, 3),
+            numeric_limits<unsigned>::max(),
+        },
+        {
+            cost_of(Mage, 1),
+            cost_of(Mage, 2),
+            cost_of(Mage, 3),
+            cost_of(Mage, 4),
+        },
+        {
+            cost_of(Healer, 1),
+            cost_of(Healer, 2),
+            cost_of(Healer, 3),
+            numeric_limits<unsigned>::max(),
+        },
+        {
+            // Can't research factories.
+            numeric_limits<unsigned>::max(),
+            numeric_limits<unsigned>::max(),
+            numeric_limits<unsigned>::max(),
+            numeric_limits<unsigned>::max(),
+        },
+        {
+            cost_of(Rocket, 1),
+            cost_of(Rocket, 2),
+            cost_of(Rocket, 3),
+            numeric_limits<unsigned>::max(),
+        },
+    }};
 
 // Pairwise distances kernels.
 // XXX: magic number from the specs.
@@ -128,7 +127,7 @@ const static vector<pair<int, int>> KERNEL[N_UNIT_TYPES] = {
 };
 
 // Attack ranges.
-const static unsigned ATTACK_RANGE[N_UNIT_TYPES] = {
+const static array<unsigned, N_UNIT_TYPES> ATTACK_RANGE = {
     0,   // Worker
     2,   // Knight
     50,  // Ranger
